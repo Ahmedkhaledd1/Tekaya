1220e2df5902deed379ad984b93fb20c9c21af7c
ABarakat27 <ahmedgalaleldine2@gmail.com>
2024-11-17 23:42:30 +0200

    retrieved the data for the organization

--- Model/Organization.php
+++ Model/Organization.php
@@ -18,10 +18,54 @@ class Organization extends AbstractUser
         $conn->query($sql);
     }
     public function addDonation(Donation $donation) {}
-}
 
+
+    public function getUserbyId(int $id){
+        // Check if email already exists in the database
+        $conn = DBConnection::getInstance()->getConnection();
+        $sql = "SELECT * FROM user WHERE id = '$id'";
+        $result = $conn->query($sql);
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            parent::__construct($row['email'],$row['password'],$row['mobile']);
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    public function getOrganizationByID(int $id) {
+        $conn = DBConnection::getInstance()->getConnection();
+        $sql = "SELECT * FROM organization WHERE user_id = '$id'";
+        $result = $conn->query($sql);
+    
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+    
+            $this->title = $row["title"];
+            $this->taxNumber = $row["tax_number"];
+            $orgTypeString = $row["orgtype"];
+            switch ($orgTypeString) {
+                case "restaurant":
+                    $this->orgType = OrgType::restaurant;
+                    break;
+                case "shop":
+                    $this->orgType = OrgType::shop;
+                    break;
+                default:
+                    throw new Exception("Invalid orgtype value: $orgTypeString");
+            }
+    
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 enum OrgType
 {
     case restaurant;
     case shop;
 }
+
